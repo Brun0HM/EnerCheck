1c2528d43139ef5149cf1e3031ec794910bb82d2
Brun0 <brunodev41@gmail.com>
2025-09-03 11:43:57 -0300

    Atualiza o componente App para incluir conformidade com NBR 5410 e adiciona arquivo SCSS para estilos personalizados

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <div className="container mt-5 text-center d-flex flex-column align-items-center justify-content-center">
+        <div>
+          <p className="bg-primary text-light fw-semibold p-2 rounded-2">
+            Conformidade com NBR 5410
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
