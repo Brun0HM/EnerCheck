a8f36095b072c323a4b70beec2af02e74fb6b937
Thiago Mazzi <thiagodev728@gmail.com>
2025-09-03 14:28:30 -0300

    Componente loginform

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import React from "react";
+import Login from "./components/Login";
 
 const App = () => {
   return (
-    <div>
-      <div className="container mt-5 text-center d-flex flex-column align-items-center justify-content-center">
-        <div>
-          <p className="bg-primary text-light fw-semibold p-2 rounded-2">
-            Conformidade com NBR 5410
-          </p>
-        </div>
-      </div>
+    <div className="d-flex justify-content-center">
+      <Login />
     </div>
   );
 };
