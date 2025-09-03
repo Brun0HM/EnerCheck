5286a0051ef9e9ae8ac9aed0d7fefbc05bbab2ff
Joaquim <jocaq124@gmail.com>
2025-09-03 17:00:38 -0300

    Fazendo o Header, falta fazer de forma a funcionar o abrir e fechar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import Header from "./components/Header";
+import "./styles/main.scss"
 
 const App = () => {
   return (
     <div>
+      <Header />
+      <div className={`mainContent`}>
       <div className="container mt-5 text-center d-flex flex-column align-items-center justify-content-center">
         <div>
           <p className="bg-primary text-light fw-semibold p-2 rounded-2">
             Conformidade com NBR 5410
           </p>
         </div>
+      </div>
       </div>
     </div>
   );
